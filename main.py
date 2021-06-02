11159d1c490cc8c0149c8ba6d596ce4ec4141b65
Jeevan <jeevanpillai31@gmail.com>
2021-06-03 00:20:44 +0800

    added 2nd query

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ query = ""
 
 while (query != "/stop"):
   query = input("Enter your search query: ")
-  words = query.split(" ")
+  words = query.lower().split(" ")
 
   str_object, str_attribute = "", ""
   image_link = ""
 
-  if("what" in words and "is" in words):    #query 1: what is the <key> of <object>   OR   what is <object>
+  if("what" in words and "is" in words):    #query 1: what is the <attribute> of <object>   OR   what is <object>
     
     if ("the" in words):
       for i in range(words.index("of")+1, len(words)):
@@ -34,11 +34,20 @@ while (query != "/stop"):
     else:
       str_attribute = "definition"
 
-    if (str_object != "" and str_attribute != ""):
 
-      print(data[str_object][str_attribute])
-    else:
-      print("query not found")
+  if("how" in words and "work" in words):    #query 2: how does <object> work
+    for i in range(words.index("does")+1, words.index("work")):
+        str_object += words[i]
+        if(i != words.index("work")-1):
+          str_object += "-"
 
+  if (str_object != "" and str_attribute != ""):
+    print("Object: ", str_object)
+    print("Attribute: ", str_attribute)    
+    print(data[str_object][str_attribute])
+    if(str_attribute+"-img" in data[str_object].keys()):
+      print("Image Link: ", data[str_object][str_attribute+"-img"])
+  else:
+    print("query not found")
 
 ##what is the definition of iterative-algorithm 
